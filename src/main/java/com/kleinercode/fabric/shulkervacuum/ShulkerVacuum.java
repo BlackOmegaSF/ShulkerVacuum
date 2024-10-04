ceb0446a217d79e0e161100419b32b94529b70a1
Kian <kian.leiner@gmail.com>
2024-10-03 20:48:33 -0700

    Initial commit

---
+++ src/main/java/com/kleinercode/fabric/shulkervacuum/ShulkerVacuum.java
@@ -0,0 +1,14 @@
+package com.kleinercode.fabric.shulkervacuum;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class ShulkerVacuum implements DedicatedServerModInitializer {
+
+    @Override
+    public void onInitializeServer() {
+
+
+
+    }
+}
